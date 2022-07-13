bc6ea2b11b2a28317757498484e9860c774f9155
mahan <m.mahmodi1380@gmail.com>
2022-07-13 14:42:23 +0430

    render to string function for home component

--- server/src/index.js
+++ server/src/index.js
@@ -1,7 +1,13 @@
 const express = require('express');
+const react = require('react');
+const renderToString = require('react-dom/server').renderToString;
+const Home = require('./client/components/Home').default;
 const app = express();
 
-app.get('/', (req, res) => {});
+app.get('/', (req, res) => {
+  const content = renderToString(<Home />);
+  res.send(content);
+});
 
 app.listen(3000, () => {
   console.log('listening on port 3000.');

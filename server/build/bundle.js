68d8a6020399aa1a8c2b978c0e788bb78668ed02
mahan <m.mahmodi1380@gmail.com>
2022-08-06 19:52:32 +0430

    chapter 3 done

--- server/build/bundle.js
+++ server/build/bundle.js
@@ -7062,7 +7062,7 @@ var app = (0, _express2.default)();
 app.use(_express2.default.static('public'));
 app.get('/', function (req, res) {
   var content = (0, _server.renderToString)(_react2.default.createElement(_Home2.default, null));
-  var html = '\n  <html>\n    <head></head>\n    <body>\n      <div>' + content + '</div>\n      <script src="bundle.js"></script>\n    </body>\n  </html>\n  ';
+  var html = '\n  <html>\n    <head></head>\n    <body>\n      <div id="root">' + content + '</div>\n      <script src="bundle.js"></script>\n    </body>\n  </html>\n  ';
   res.send(html);
 });
 

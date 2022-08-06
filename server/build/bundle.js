75e4bf295dd9277ddfc5a6eb489f0726b33a8656
mahan <m.mahmodi1380@gmail.com>
2022-08-06 19:33:11 +0430

    html with bundle.js script sent to browser.

--- server/build/bundle.js
+++ server/build/bundle.js
@@ -7059,10 +7059,11 @@ var _Home2 = _interopRequireDefault(_Home);
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 var app = (0, _express2.default)();
-
+app.use(_express2.default.static('public'));
 app.get('/', function (req, res) {
   var content = (0, _server.renderToString)(_react2.default.createElement(_Home2.default, null));
-  res.send(content);
+  var html = '\n  <html>\n    <head></head>\n    <body>\n      <div>' + content + '</div>\n      <script src="bundle.js"></script>\n    </body>\n  </html>\n  ';
+  res.send(html);
 });
 
 app.listen(3000, function () {
--- server/src/index.js
+++ server/src/index.js
@@ -4,10 +4,19 @@ import { renderToString } from 'react-dom/server';
 import Home from './client/components/Home';
 
 const app = express();
-
+app.use(express.static('public'));
 app.get('/', (req, res) => {
   const content = renderToString(<Home />);
-  res.send(content);
+  const html = `
+  <html>
+    <head></head>
+    <body>
+      <div>${content}</div>
+      <script src="bundle.js"></script>
+    </body>
+  </html>
+  `;
+  res.send(html);
 });
 
 app.listen(3000, () => {

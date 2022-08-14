57f0cbf76e63e049258874ebd785e0c0d5313d3f
mahan <m.mahmodi1380@gmail.com>
2022-08-14 22:00:53 +0430

    reducers import

--- server/src/client/client.js
+++ server/src/client/client.js
@@ -5,6 +5,7 @@ import Routes from './Routes';
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import { Provider } from 'react-redux';
+import reducers from './reducers';
 
 const store = createStore(reducers, {}, applyMiddleware(thunk));
 

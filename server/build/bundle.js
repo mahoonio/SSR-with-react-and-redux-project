7b194e144aa016482eb06bb0877de9b34cc187f5
mahan <m.mahmodi1380@gmail.com>
2022-08-17 22:42:29 +0430

    proxy setup.

--- server/build/bundle.js
+++ server/build/bundle.js
@@ -200,9 +200,19 @@ var _Routes = __webpack_require__(1);
 
 var _Routes2 = _interopRequireDefault(_Routes);
 
+var _expressHttpProxy = __webpack_require__(20);
+
+var _expressHttpProxy2 = _interopRequireDefault(_expressHttpProxy);
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 var app = (0, _express2.default)();
+app.use('/api', (0, _expressHttpProxy2.default)('http://react-ssr-api.herokuapp.com', {
+  proxyReqOptDecorator: function proxyReqOptDecorator(opts) {
+    opts.header['x-forwarded-host'] = 'localhost:3000'; // this options are just for course purposes.
+    return opts;
+  }
+}));
 app.use(_express2.default.static('public'));
 app.get('*', function (req, res) {
   var store = (0, _createStore2.default)();
@@ -512,6 +522,12 @@ exports.default = function () {
       return state;
   }
 };
+
+/***/ }),
+/* 20 */
+/***/ (function(module, exports) {
+
+module.exports = require("express-http-proxy");
 
 /***/ })
 /******/ ]);
--- server/src/index.js
+++ server/src/index.js
@@ -4,7 +4,17 @@ import renderer from './helpers/renderer';
 import createStore from './helpers/createStore';
 import { matchRoutes } from 'react-router-config';
 import Routes from './client/Routes';
+import proxy from 'express-http-proxy';
 const app = express();
+app.use(
+  '/api',
+  proxy('http://react-ssr-api.herokuapp.com', {
+    proxyReqOptDecorator(opts) {
+      opts.header['x-forwarded-host'] = 'localhost:3000'; // this options are just for course purposes.
+      return opts;
+    },
+  })
+);
 app.use(express.static('public'));
 app.get('*', (req, res) => {
   const store = createStore();

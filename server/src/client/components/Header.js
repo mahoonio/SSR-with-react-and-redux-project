b4053917822d8277497d65988700f7c547d35150
mahan <m.mahmodi1380@gmail.com>
2022-08-25 19:43:14 +0430

    Header Customization.

--- server/src/client/components/Header.js
+++ server/src/client/components/Header.js
@@ -3,9 +3,19 @@ import { Link } from 'react-router-dom';
 import { connect, Connect } from 'react-redux';
 const Header = ({ auth }) => {
   console.log('the auth status : ', auth);
+  const authButton = auth ? (
+    <a href="/api/logout">Logout</a>
+  ) : (
+    <a href="/api/auth/google">Login</a>
+  );
   return (
     <div>
       <Link to={'/'}>React SSR</Link>
+      <div>
+        <Link to={'/users'}>Users</Link>
+        <Link to={'/admins'}>Admins</Link>
+        {authButton}
+      </div>
     </div>
   );
 };
